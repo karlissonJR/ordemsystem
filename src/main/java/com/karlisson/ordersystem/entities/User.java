97612470f5bdb7b64de8c8f7101853648249e183
karlissonJR <karlissonj@gmail.com>
2022-09-21 08:52:05 -0300

    configuração e injeção de dados no ambiente de teste

--- src/main/java/com/karlisson/ordersystem/entities/User.java
+++ src/main/java/com/karlisson/ordersystem/entities/User.java
@@ -7,10 +7,13 @@ import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
---
+++ src/main/java/com/karlisson/ordersystem/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.karlisson.ordersystem.repositories;
+
+import com.karlisson.ordersystem.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+
+}
