6cf75124b282b405d731ad687845f6d73b9e9e36
karlissonJR <karlissonj@gmail.com>
2022-09-21 07:55:42 -0300

    first commit

---
+++ src/test/java/com/karlisson/ordersystem/OrdersystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karlisson.ordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
