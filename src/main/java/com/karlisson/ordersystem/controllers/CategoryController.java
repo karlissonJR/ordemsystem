fc1f2b754385231f7edb97d3b4c8318009cbbf90
karlissonJR <karlissonj@gmail.com>
2022-09-21 11:13:10 -0300

    rotas iniciais para categorias

---
+++ src/main/java/com/karlisson/ordersystem/controllers/CategoryController.java
@@ -0,0 +1,34 @@
+package com.karlisson.ordersystem.controllers;
+
+import com.karlisson.ordersystem.entities.Category;
+import com.karlisson.ordersystem.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll() {
+        List<Category> categories = categoryService.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(categories);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> findById(@PathVariable Long id) {
+        Category category = categoryService.findById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(category);
+    }
+
+}
